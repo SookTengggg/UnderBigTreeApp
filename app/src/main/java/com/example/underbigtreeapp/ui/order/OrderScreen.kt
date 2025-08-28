ff0aca5a386c138fd2628d0ba9fb72abb8140e18
Sook Teng <stpun18@gmail.com>
2025-08-28 17:13:22 +0800

    order page

--- app/src/main/java/com/example/underbigtreeapp/ui/order/OrderScreen.kt
+++ app/src/main/java/com/example/underbigtreeapp/ui/order/OrderScreen.kt
@@ -1,2 +1,16 @@
 package com.example.underbigtreeapp.ui.order
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun OrderScreen(){
+
+}
